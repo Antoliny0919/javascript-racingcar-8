8c174070b3baa70a852428ce5b806a2e03413f6b
antoliny0919 <antoliny0919@gmail.com>
2025-10-21 18:07:27 +0900

    feat(app): 자동차 이름 입력, 입력 예외 처리 추가
    자동차 이름 입력 로직을 추가했습니다:
    - 자동차 이름 입력받는 로직 추가
    - ','기준으로 자동차 이름 분리
    
    자동차 이름 입력 예외 케이스 추가했습니다:
    - 입력을 안했을때 예외 처리 추가
    - 자동차 이름이 공백일때 예외 처리 추가
    - 자동차 이름이 5자 이하가 아닐때 예외 처리 추가
    - 동일한 자동차 이름이 존재할때 예외 처리 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+
+  validateCarName(carNameList) {
+    if (carNameList.length === 0) {
+      throw new Error('[ERROR] 자동차 이름을 입력하지 않았습니다.')
+    }
+    if (carNameList.some(name => name.trim() === '')) {
+      throw new Error('[ERROR] 공백은 자동차 이름으로 사용할 수 없습니다.')
+    }
+    if (carNameList.some(name => name.length > 5)) {
+      throw new Error('[ERROR] 자동차 이름은 5자 이하여야 합니다.')
+    }
+    if (new Set(carNameList).length !== carNameList.length) {
+      throw new Error('[ERROR] 동일한 이름은 사용할 수 없습니다.')
+    }
+  }
+
+  async run() {
+    const carNames = await Console.readLineAsync('경주할 자동차 이름(이름은 쉼표(,) 기준으로 구분');
+    const carNameList = carNames.split(',');
+    this.validateCarName(carNameList)
+  }
 }
 
 export default App;
